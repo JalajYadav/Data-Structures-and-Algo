76bc1577c1710d3eeb83ae6f1dcf1d46549b4b6b
JalajYadav <jalajyadav99@gmail.com>
2022-05-24 21:51:12 +0530

    Tuesday, 24 May 2022

---
+++ DSA Restart/Competitive/Binary Search/287problemB.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v)
+{
+    os << '{';
+    string sep;
+    for (const T &x : v)
+        os << sep << x, sep = ", ";
+    return os << '}';
+}
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define pb push_back
+#define mk make_pair
+#define umap unordered_map
+#define uset unordered_set
+#define imax INT_MAX
+#define imin INT_MIN
+#define rep(i, n) for (i = 0; i < n; ++i)
+#define REP(i, k, n) for (i = k; i <= n; ++i)
+#define REPR(i, k, n) for (i = k; i >= n; --i)
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+ll n, k;
+
+ll sumA(ll x)
+{
+    return (x * (x + 1)) / 2;
+}
+
+ll sumA(ll mid, ll end)
+{
+    if (mid <= 1)
+        return sumA(end);
+
+    return (sumA(end) - sumA(mid - 1));
+}
+
+void solve()
+{
+    cin >> n;
+    cin >> k;
+
+    if (n == 1)
+    {
+        cout << 0;
+        return;
+    }
+
+    if (n <= k)
+    {
+        cout << 1;
+        return;
+    }
+
+    --k;
+    --n;
+
+    if (sumA(k) < n)
+    {
+        cout << -1 << "\n";
+        return;
+    }
+
+    ll st = 1, en = k;
+
+    while (st < en)
+    {
+        ll md = (st + en) / 2;
+        ll s = sumA(md, k);
+
+        if (s == n)
+        {
+            cout << (k - md + 1);
+            return;
+        }
+        if (s > n)
+            st = md + 1;
+        else
+            en = md;
+    }
+
+    cout << (k - st + 2);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++)
+    {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+}
